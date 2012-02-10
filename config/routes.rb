83e8165a5806c10ad956ef29b9d359d5ee30dc27
Miguel Bejar <bejar37@gmail.com>
2012-02-09 20:52:09 -0500

    Finish layout and changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
 
-  match '/help' to: 'static_pages#help'
+  get "users/new"
 
-  match '/about' to: 'static_pages#about'
+  match '/signup', to: 'users#new'
 
-  match '/contact' to: 'static_pages#contact'
->>>>>>> filling-in-layout
+  match '/help', to: 'static_pages#help'
+
+  match '/about', to: 'static_pages#about'
+
+  match '/contact', to: 'static_pages#contact'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -56,7 +58,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'static_pages/home'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,59 +1,34 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "Home Page" do
-    it "should have the h1 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit root_path
-      page.should have_selector('title', 
-                                :text => 'Sample App') 
+describe "Static pages" do
+
+    subject { page }
+
+    describe "Home page" do
+      before { visit root_path }
+
+      it { should have_selector('h1',    text: 'Sample App') }
+      it { should have_selector('title', text: full_title('Home')) }
     end
 
-    it "should have the title 'Home'" do
-      visit root_path
-      page.should have_selector('title',
-                                :text => " | Home")
-    end
-  end
+    describe "Help page" do
+      before { visit help_path }
 
-  describe "Help page" do
-
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
+      it { should have_selector('h1',    text: 'Help') }
+      it { should have_selector('title', text: full_title('Help')) }
     end
 
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-                                :text => " | Help")
-    end
-  end
+    describe "About page" do
+      before { visit about_path }
 
-  describe "About page" do
-
-    it "should have the h1 'About'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
+      it { should have_selector('h1',    text: 'About') }
+      it { should have_selector('title', text: full_title('About Us')) }
     end
 
-    it "should have the title 'About'" do
-      visit about_path
-      page.should have_selector('title',
-                                :text => " | About Us")
-    end
-  end
+    describe "Contact page" do
+      before { visit contact_path }
 
-  describe "Contact Page" do
-
-    it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
+      it { should have_selector('h1',    text: 'Contact') }
+      it { should have_selector('title', text: full_title('Contact')) }
     end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => " | Contact")
-    end
-  end
 end
