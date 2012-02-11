74162d5a04238a8e98733713b65c425c8085b1bf
Miguel Bejar <bejar37@gmail.com>
2012-02-11 12:11:45 -0500

    Finish User Signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120210152110) do
+ActiveRecord::Schema.define(:version => 20120211170731) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,7 +20,5 @@ ActiveRecord::Schema.define(:version => 20120210152110) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Miguel Bejar"
+    email     "bejar37@gmail.com"
+    password  "foobar"
+  end
+end
